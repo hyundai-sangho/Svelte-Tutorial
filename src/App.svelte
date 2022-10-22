37bbe5294cf7bc56eff931ec6da7484f4a228aa8
조상호 <hyundai_sangho@naver.com>
2022-10-22 13:15:22 +0900

    선언적 렌더링 학습

---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+  let name = 'world';
+  let age = 85;
+
+  function assign() {
+    name = 'cho';
+    age = 990;
+  }
+</script>
+
+<h1>Hello {name}</h1>
+<h2 class={age < 85 ? 'active' : ''}>{age}</h2>
+
+<img src="" alt={name} />
+
+<!-- 양방향 바인딩은 bind: 을 붙인다. -->
+<input type="text" bind:value={name} />
+<button on:click={assign}>Assign</button>
+
+<style>
+  h1 {
+    color: red;
+  }
+
+  .active {
+    color: blue;
+  }
+</style>
